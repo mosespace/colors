41a79b46f812581833779e216bcc24b476aae376
mosespace <kiskayemoses@gmail.com>
2026-02-06 20:25:27 +0000

    fix: vercel deployment error

--- lib/auth.ts
+++ lib/auth.ts
@@ -47,7 +47,7 @@ if (!db) {
   throw new Error('❌ Database client not initialized');
 }
 
-// const resend = new Resend(process.env.RESEND_API_KEY as any);
+const resend = new Resend(process.env.RESEND_API_KEY as any);
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -60,147 +60,147 @@ export const authOptions: NextAuthOptions = {
     error: '/login',
     verifyRequest: '/verify-request', // Used for check email page
   },
-  // providers: [
-  //   {
-  //     id: 'resend',
-  //     type: 'email',
-  //     role: 'USER',
-  //     maxAge: 5 * 60, // 5 minutes
-  //     sendVerificationRequest: async (params: any) => {
-  //       const { identifier: email, url } = params;
-  //       const isDevelopment = process.env.NODE_ENV === 'development';
-  //       try {
-  //         if (isDevelopment) {
-  //           console.log(`🪄 Magic Link: ${url}`);
-  //           return;
-  //         }
+  providers: [
+    {
+      id: 'resend',
+      type: 'email',
+      role: 'USER',
+      maxAge: 5 * 60, // 5 minutes
+      sendVerificationRequest: async (params: any) => {
+        const { identifier: email, url } = params;
+        const isDevelopment = process.env.NODE_ENV === 'development';
+        try {
+          if (isDevelopment) {
+            console.log(`🪄 Magic Link: ${url}`);
+            return;
+          }
 
-  //         await resend.emails.send({
-  //           from: `${siteConfig.name} <welcome@mosespace.com>`,
-  //           to: [email],
-  //           subject: `Your ${process.env.NEXT_PUBLIC_APP_NAME} Login Link | Expires in 10 minutes!!!`,
-  //           react: LoginLink({ url, email }),
-  //         });
+          await resend.emails.send({
+            from: `${siteConfig.name} <welcome@mosespace.com>`,
+            to: [email],
+            subject: `Your ${process.env.NEXT_PUBLIC_APP_NAME} Login Link | Expires in 10 minutes!!!`,
+            react: LoginLink({ url, email }),
+          });
 
-  //         console.log(`✅ Verification email sent successfully to: ${email}`);
-  //       } catch (error) {
-  //         console.error('❌ Error sending verification email:', error);
-  //         throw new Error('Error sending verification email');
-  //       }
-  //     },
-  //   } as any,
-  //   GitHubProvider({
-  //     clientId: process.env.GITHUB_CLIENT_ID || '',
-  //     clientSecret: process.env.GITHUB_SECRET || '',
-  //     profile(profile) {
-  //       return {
-  //         id: profile.id.toString(),
-  //         name: profile.name || profile.login,
-  //         role: 'USER',
-  //         email: profile.email,
-  //         image: profile.avatar_url,
-  //         isMachine: false,
-  //         invalidLoginAttempts: 0,
-  //         createdAt: new Date(),
-  //         updatedAt: new Date(),
-  //       };
-  //     },
-  //   }),
-  //   GoogleProvider({
-  //     clientId: process.env.GOOGLE_CLIENT_ID || '',
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-  //     profile(profile) {
-  //       return {
-  //         id: profile.sub,
-  //         name: `${profile.given_name} ${profile.family_name}`,
-  //         email: profile.email,
-  //         role: 'USER',
-  //         image: profile.picture,
-  //         isMachine: false,
-  //         invalidLoginAttempts: 0,
-  //         createdAt: new Date(),
-  //         updatedAt: new Date(),
-  //       };
-  //     },
-  //   }),
-  //   CredentialsProvider({
-  //     name: 'Credentials',
-  //     credentials: {
-  //       email: { label: 'Email', type: 'email' },
-  //       password: { label: 'Password', type: 'password' },
-  //     },
+          console.log(`✅ Verification email sent successfully to: ${email}`);
+        } catch (error) {
+          console.error('❌ Error sending verification email:', error);
+          throw new Error('Error sending verification email');
+        }
+      },
+    } as any,
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          role: 'USER',
+          email: profile.email,
+          image: profile.avatar_url,
+          isMachine: false,
+          invalidLoginAttempts: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: `${profile.given_name} ${profile.family_name}`,
+          email: profile.email,
+          role: 'USER',
+          image: profile.picture,
+          isMachine: false,
+          invalidLoginAttempts: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      },
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
 
-  //     // @ts-ignore
-  //     async authorize(credentials) {
-  //       try {
-  //         if (!credentials?.email || !credentials?.password) {
-  //           throw new Error('Missing credentials');
-  //         }
+      // @ts-ignore
+      async authorize(credentials) {
+        try {
+          if (!credentials?.email || !credentials?.password) {
+            throw new Error('Missing credentials');
+          }
 
-  //         const environment = process.env.NODE_ENV;
+          const environment = process.env.NODE_ENV;
 
-  //         if (environment === 'development') {
-  //           console.log('Skipped ratelimit in development');
-  //           const { success } = await ratelimit.limit(credentials.email);
-  //           if (!success) {
-  //             throw new Error('Too many login attempts');
-  //           }
-  //         }
-  //         // Rate limiting check
+          if (environment === 'development') {
+            console.log('Skipped ratelimit in development');
+            const { success } = await ratelimit.limit(credentials.email);
+            if (!success) {
+              throw new Error('Too many login attempts');
+            }
+          }
+          // Rate limiting check
 
-  //         // Find user
-  //         const user = await db.user.findUnique({
-  //           where: { email: credentials.email },
-  //         });
+          // Find user
+          const user = await db.user.findUnique({
+            where: { email: credentials.email },
+          });
 
-  //         // console.log('User:', user);
+          // console.log('User:', user);
 
-  //         if (!user || !user.passwordHash) {
-  //           throw new Error('Invalid credentials');
-  //         }
+          if (!user || !user.passwordHash) {
+            throw new Error('Invalid credentials');
+          }
 
-  //         // Check if account is locked
-  //         if (user.lockedAt) {
-  //           throw new Error('Account is locked');
-  //         }
+          // Check if account is locked
+          if (user.lockedAt) {
+            throw new Error('Account is locked');
+          }
 
-  //         // Check login attempts
-  //         if (exceededLoginAttemptsThreshold(user)) {
-  //           await lockAccount(user.id);
-  //           throw new Error('Account locked due to too many attempts');
-  //         }
+          // Check login attempts
+          if (exceededLoginAttemptsThreshold(user)) {
+            await lockAccount(user.id);
+            throw new Error('Account locked due to too many attempts');
+          }
 
-  //         // Validate password
-  //         const isValid = await validatePassword({
-  //           password: credentials.password,
-  //           passwordHash: user.passwordHash,
-  //         });
+          // Validate password
+          const isValid = await validatePassword({
+            password: credentials.password,
+            passwordHash: user.passwordHash,
+          });
 
-  //         if (!isValid) {
-  //           await incrementLoginAttempts(user.id);
-  //           throw new Error('Invalid credentials');
-  //         }
+          if (!isValid) {
+            await incrementLoginAttempts(user.id);
+            throw new Error('Invalid credentials');
+          }
 
-  //         // Reset login attempts on successful login
-  //         await db.user.update({
-  //           where: { id: user.id },
-  //           data: { invalidLoginAttempts: 0 },
-  //         });
+          // Reset login attempts on successful login
+          await db.user.update({
+            where: { id: user.id },
+            data: { invalidLoginAttempts: 0 },
+          });
 
-  //         return {
-  //           id: user.id.toString(),
-  //           name: user.name,
-  //           email: user.email,
-  //           image: user.image,
-  //           role: user.role,
-  //         };
-  //       } catch (error) {
-  //         console.error('Auth error:', error);
-  //         throw error;
-  //       }
-  //     },
-  //   }),
-  // ],
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            role: user.role,
+          };
+        } catch (error) {
+          console.error('Auth error:', error);
+          throw error;
+        }
+      },
+    }),
+  ],
   callbacks: {
     async jwt({ token, user, trigger, session }) {
       if (user) {
@@ -264,22 +264,23 @@ export const authOptions: NextAuthOptions = {
     },
   },
   events: {
-    // async signIn({ user, isNewUser }) {
-    //   if (isNewUser) {
-    //     // Send welcome email
-    //     await resend.emails.send({
-    //       from: '${siteConfig.name} <welcome@mosespace.com>',
-    //       to: [user.email],
-    //       subject: `Welcome to ${siteConfig.name}!`,
-    //       react: WelcomeEmail({
-    //         email: user.email,
-    //         name: user.name || null,
-    //       }),
-    //       // send the welcome email 5 minutes after the user signed up
-    //       scheduledAt: new Date(Date.now() + 5 * 60 * 1000).toISOString(),
-    //     });
-    //   }
-    // },
+    async signIn({ user, isNewUser }) {
+      if (isNewUser) {
+        // Send welcome email
+        await resend.emails.send({
+          from: '${siteConfig.name} <welcome@mosespace.com>',
+          to: [user.email],
+          subject: `Welcome to ${siteConfig.name}!`,
+          react: WelcomeEmail({
+            email: user.email,
+            name: user.name || null,
+          }),
+
+          // send the welcome email 5 minutes after the user signed up
+          scheduledAt: new Date(Date.now() + 5 * 60 * 1000).toISOString(),
+        });
+      }
+    },
   },
   debug: process.env.NODE_ENV === 'development',
 };
